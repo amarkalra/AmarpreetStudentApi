8a989f4bf84a7f40497c6ceb789a3a64f1f04fe2
amarkalra <amarpreet.kalra@gmail.com>
2016-09-08 07:34:48 -0400

    Added JSON Consumer for adding a student

--- src/main/java/com/capone/controller/StudentResource.java
+++ src/main/java/com/capone/controller/StudentResource.java
@@ -1,10 +1,16 @@
 package com.capone.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,6 +41,15 @@ public class StudentResource {
 		return new Student();
 	}
 
+	@RequestMapping(value = "/postStudent", method = RequestMethod.POST, consumes = "application/json")
+	String saveStudent(@RequestBody Student std) {
+		if (std != null) {
+			studentService.saveStudent(std);
+		}
+		
+		return "Student added successfully";
+	}
+
 	@RequestMapping("/uploadStudentForm")
 	public String displayUploadPage() {
 		return "UploadPage";
@@ -44,6 +59,7 @@ public class StudentResource {
 	public String handleFileUpload(
 			@RequestParam("fileUpload") MultipartFile file) {
 
+		
 		ArrayList<Student> studentList = CSVParserUtil.parserStudentCSV(file);
 
 		studentService.saveStudents(studentList);
@@ -57,4 +73,10 @@ public class StudentResource {
 		Student std = studentService.getStudentById(id);
 		return std;
 	}
+	
+	@ExceptionHandler
+	void handleIllegalArgumentException(IllegalArgumentException e, HttpServletResponse response) throws IOException {
+	    response.sendError(HttpStatus.BAD_REQUEST.value(),"File is empty, please submit again");
+	}
+	
 }
--- src/test/java/com/capone/SampleStudentApiApplicationTests.java
+++ src/test/java/com/capone/SampleStudentApiApplicationTests.java
@@ -16,6 +16,7 @@ public class SampleStudentApiApplicationTests {
 	
 	@Test
 	public void myTest() {
+	    
 	}
 
 }
